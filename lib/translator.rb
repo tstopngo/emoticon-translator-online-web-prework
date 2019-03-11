29f936807baeb117544b374dc3d59cb5116f73dd
Tracy Ngo <tngo1206@gmail.com>
2019-03-11 00:08:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,7 +8,6 @@ def load_library(path)
   emojis_hash = {["get_emoticon"] = {},emojis_hash["get_meaning"]  = {}}
   
   emojis.each do |meaning, emotes|
-
   emojis_hash["get_emoticon"][emotes[0]] = emotes[1] 
   emojis_hash["get_meaning"][emotes[1]] = meaning
   end
